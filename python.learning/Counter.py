72a3185c3aee779c2cf7ac8239ad7acd896c162b
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2021-05-26 09:54:46 -0300

    2 files py.learn add

---
+++ python.learning/Counter.py
@@ -0,0 +1,9 @@
+# What will be the output?:
+
+counter = input("Write something: ")
+
+# exit1:
+print(f"The counter is {counter}")
+
+# exit2:
+print("this is counter {0}".format(counter))
---
+++ python.learning/test-function.py
@@ -0,0 +1,8 @@
+# rewriting functions:
+
+def var(variable):
+    if variable >= 10:
+        return True
+    return False
+
+print(var(1.2))
