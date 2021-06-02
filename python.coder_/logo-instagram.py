f48bcf7a1aaa80a4cc57e7476f841695cddce569
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2021-06-02 10:19:54 -0300

    logo-instagram add

---
+++ python.coder_/logo-instagram.py
@@ -0,0 +1,22 @@
+from turtle import*
+
+bgcolor('purple') #
+pencolor('yellow')
+width(25) #
+penup()
+goto(160, -100)
+pendown()
+
+left(90)
+for i in range(4):
+    forward(250)
+    circle(35, 90) #
+
+penup()
+goto(85, 30)
+pendown()
+circle(80, 360)
+penup()
+goto(110, 130)
+pendown()
+circle(10, 360) #
