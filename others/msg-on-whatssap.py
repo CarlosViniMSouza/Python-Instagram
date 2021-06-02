b97d008ead1c26da9049960f87e7cbfe27108b5f
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2021-06-02 11:13:35 -0300

    TODOs add

--- others/msg-on-whatssap.py
+++ others/msg-on-whatssap.py
@@ -3,3 +3,5 @@
 import pywhatkit
 
 kit.sendwhatmsg("*receiver phone*", "Python is very good for tasks automation", 22,30)
+
+# FIXME resolver problema aqui
--- others/python-tip-n1.py
+++ others/python-tip-n1.py
@@ -1,2 +1,4 @@
 a, b, c = input("Writing 3 words separte by space: ").split()
 print("So, we have: \n", a+"\n", b+"\n", c+"\n")
+
+# TODO resolver aqui
