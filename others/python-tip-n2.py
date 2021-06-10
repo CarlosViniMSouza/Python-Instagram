a779564d9440e21dc101baad81295e4e85057573
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2021-06-10 11:07:24 -0300

    python trip n2 defect

---
+++ others/python-tip-n2.py
@@ -0,0 +1,8 @@
+print("\033[01m_Python_Trip_in_colours")
+print("\033[96m_Python_Trip_in_colours")
+print("\033[95m_Python_Trip_in_colours")
+print("\033[94m_Python_Trip_in_colours")
+print("\033[93m_Python_Trip_in_colours")
+print("\033[92m_Python_Trip_in_colours")
+
+# this isn't working correctly
