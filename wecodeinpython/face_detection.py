95b833febf25012a0a62bdfb52267eaa752737f9
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2021-06-10 10:55:10 -0300

    alt doing

--- wecodeinpython/face_detection.py
+++ wecodeinpython/face_detection.py
@@ -16,17 +16,17 @@ gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 # Detect faces in the image:
 faces = faceCascade.detectMultiScale(
     gray,
-    scaleFactor = 1.1,
-    minNeighbors = 5,
-    minSize = (30, 30),
+    scaleFactor = 1.5,
+    minNeighbors = 10,
+    minSize = (45, 45),
     flags = cv2.cv.CV_HAAR_SCALE_IMAGE
 )
 
 print("Found {0} faces!".format(len(faces)))
 
 # Draw a rectangle around the faces:
-for(x, y, w, h) in faces:
-    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+for(a, b, c, d) in faces:
+    cv2.rectangle(image, (a, b), (a+c, b+d), (0, 255, 0), 2)
 
 cv2.imshow("Faces found", image)
 cv2.waitKey(0)
