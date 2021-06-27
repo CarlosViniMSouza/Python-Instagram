94c0b181914483f1c990e567dc713933cfae9a8b
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2021-06-27 18:51:53 -0300

    fix error

--- python.hub/Matrix.py
+++ python.hub/Matrix.py
@@ -6,13 +6,13 @@ m1 = np.array([[2, 5], [-4, 3]])
 m2 = np.array([[1, -3], [2, -6]])
 
 #1 - Method add() for add matrices:
-print("Matrix Final: \n", np.array.add(m1, m2))
+print("Matrix Final: \n", np.add(m1, m2))
 
 #2 - Method subtract() for subtract matrices:
-print("Matrix Final: \n", np.array.subtract(m1, m2))
+print("Matrix Final: \n", np.subtract(m1, m2))
 
 # 3 - Method multiply() for multiply matrices:
-print("Matrix Final: \n", np.array.multiply(m1, m2))
+print("Matrix Final: \n", np.multiply(m1, m2))
 
 # 4 - Method divide() for divide matrices:
-print("Matrix Final: \n", np.array.divide(m1, m2))
+print("Matrix Final: \n", np.divide(m1, m2))
