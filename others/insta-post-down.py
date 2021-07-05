a9ffe2a0314863c256038ed0cf3a6bb3b64b8a72
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2021-07-04 21:16:20 -0300

    files alt

--- others/insta-post-down.py
+++ others/insta-post-down.py
@@ -3,9 +3,9 @@ from instaloader import Post
 
 var = ins.Instaloader()
 
-var.login("CarlosViniMSouza", "carlinhos2020")  # (login)
-var.interactive_login("CarlosViniMSouza")      # (ask password on terminal)
-var.load_session_from_file("CarlosViniMSouza")
+var.login("", "")  # (login)
+var.interactive_login("")      # (ask password on terminal)
+var.load_session_from_file("")
 # (load session created w/ `instaloader -l USERNAME`)
 
 post = Post.from_shortcode(var.context, "CQ36GDmNTKX")
