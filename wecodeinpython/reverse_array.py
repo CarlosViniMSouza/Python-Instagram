1574a66dc3db693f3940aea544d9455adbe7a2a1
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2021-07-10 14:00:22 -0300

    reverse array add

---
+++ wecodeinpython/reverse_array.py
@@ -0,0 +1,12 @@
+nums = [1, 4, 16, 64, 256]
+
+print("Our Original Array:")
+for i in nums:
+    print(i, end = " ")
+
+nums_inv = nums[::-1]
+
+print("\n")
+print("Our Reverse Array:")
+for j in nums_inv:
+    print(j, end = " ")
