a88353ea95b719d66f72c112793a211d7ebad209
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2021-07-17 10:47:59 -0300

    email-check add

---
+++ python.coder_/email-verification.py
@@ -0,0 +1,11 @@
+# pip install verify_email -> Method1
+
+from verify_email import verify_email
+
+print(verify_email("your_email@domain.com"))
+
+# pip install validate_email -> Method2
+
+from validate_email import validate_email
+
+print(validate_email("your_email@domain.com"))
