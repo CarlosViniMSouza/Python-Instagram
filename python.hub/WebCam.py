c1566df45480945e989a61d127881910ce679b45
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2021-07-23 19:38:47 -0300

    webcam.py alts

--- python.hub/WebCam.py
+++ python.hub/WebCam.py
@@ -2,11 +2,11 @@ import cv2
 
 vid = cv2.VideoCapture(0)
 
-while(True):
-    ret, frame = vid.read()
-    cv2.imshow('frame', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+ret, frame = vid.read()
+cv2.imshow('frame', frame)
+
+if cv2.waitKey(1) & 0xFF == ord('q'):
+    print("Session Stopped")
 
 vid.release()
 cv2.destroyAllWindows()
