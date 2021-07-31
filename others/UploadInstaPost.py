8e4510ab60351c967e8c61e8160baf26d937831d
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2021-07-31 11:13:37 -0300

    instabot upload add

---
+++ others/UploadInstaPost.py
@@ -0,0 +1,11 @@
+# pip install instabot
+
+from instabot import Bot
+
+bot.login(username = "<your_username>", 
+            password = "<your_password>")
+
+bot.upload_photo("Your_Post.jpg", 
+                    caption = "Caption of post here")
+
+# post been upload ...
