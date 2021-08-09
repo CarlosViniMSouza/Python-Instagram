561fc1c39f67d35d25bca86b2bc8a68acd10f1e1
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2021-08-09 18:20:03 -0300

    error in braches

---
+++ others/hack_cell.py
@@ -0,0 +1,13 @@
+# pip install phonenumbers
+
+import phonenumbers
+
+form phonenumbers import carrier
+
+phome_num = phonenumbers.parse('<Write_with_country_code>')
+
+print(geocoder.description_for_number(phone_num, 'pt'))
+
+print(carrier.name_of_number(phome_num, 'pt'))
+
+print(timezone.time_zones_for_number(phome_num))
